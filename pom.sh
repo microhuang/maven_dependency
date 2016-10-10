0a470aa2d73d43eb2fae48f40312d17bbf01bb64
think <microhuang@live.com>
2016-10-11 03:11:49 +0800

    导出依赖树

---
+++ pom.sh
@@ -0,0 +1,30 @@
+
+pom="pom.xml"
+if [ ! -f "$pom" ]; then
+  echo "./pom.xml不存在，请检查路径是否正确！"
+  exit
+fi
+
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home "/Applications/NetBeans/NetBeans 8.1.app/Contents/Resources/NetBeans/java/maven/bin/mvn" dependency:tree -l=/tmp/pom.txt
+
+exit
+
+#cat /tmp/pom.txt | grep ":jar:" | grep -v -E "^[a-zA-Z]" | sed 's/[^a-zA-Z]*//' | sort | uniq > /tmp/pom.xml
+
+#cat /tmp/pom.txt | grep ":jar:" | grep -v -E "^[a-zA-Z]" | sed 's/[^a-zA-Z]*//' | cut -d: -f 2 | sort | uniq > /tmp/pom.xml
+
+sed -i '' '/[a-zA-Z]/i\
+<dependency>
+' /tmp/pom.txt
+
+sed -i '' '/[a-zA-Z]/a\
+</dependency>
+' /tmp/pom.txt
+
+sed -i '' "1 i\
+<dependencyManagement><dependencies>
+" /tmp/pom.txt
+
+sed -i '' "$ a\
+</dependencies></dependencyManagement>
+" /tmp/pom.txt
